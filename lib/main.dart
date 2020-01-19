f22d7bb0f30572fbf9da24ac27e3ac155ddcbf68
jesshaw <jesshaw@126.com>
2020-01-19 16:19:43 +0800

    modify the app tile

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Reservation Room Home Page'),
+      home: MyHomePage(title: 'Reservation Room Home Page!'),
     );
   }
 }
