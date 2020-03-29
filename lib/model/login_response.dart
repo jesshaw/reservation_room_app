d42837731b77300ddd86fc0a7c4e64e6756ea927
jes <jesshaw@126.com>
2020-03-30 00:20:10 +0800

    call remote api

---
+++ lib/model/login_response.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/material.dart';
+
+class LoginResponse {
+  String status;
+  LoginData data;
+  LoginResponse({this.status, this.data});
+
+  LoginResponse.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        data = LoginData.fromJson(json['data']);
+}
+
+class LoginData {
+  final String authToken;
+  final String userId;
+  const LoginData({@required this.authToken, @required this.userId});
+
+  LoginData.fromJson(Map<String, dynamic> json)
+      : authToken = json['authToken'],
+        userId = json['userId'];
+}
---
+++ lib/services/network_service_response.dart
@@ -0,0 +1,15 @@
+class NetworkServiceResponse<T> {
+  T content;
+  bool success;
+  String message;
+
+  NetworkServiceResponse({this.content, this.success, this.message});
+}
+
+class MappedNetworkServiceResponse<T> {
+  dynamic mappedResult;
+  NetworkServiceResponse<T> networkServiceResponse;
+
+  MappedNetworkServiceResponse(
+      {this.mappedResult, this.networkServiceResponse});
+}
