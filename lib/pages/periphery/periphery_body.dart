964f2ee4b39813cf6e9e22d5ff999e7628064a6f
jesshaw <jesshaw@126.com>
2020-04-12 01:17:15 +0800

    show images via staggered grid view

--- lib/pages/periphery/periphery_body.dart
+++ lib/pages/periphery/periphery_body.dart
@@ -1,65 +1,26 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:reservationroomapp/models/attraction.dart';
 import 'package:transparent_image/transparent_image.dart';
+
 
 class PeripheryBody extends StatelessWidget {
   final List<Attraction> attractions;
 
-  PeripheryBody({Key key, this.attractions}) : super(key: key);
+  PeripheryBody({Key key, this.attractions})
+      : _sizes = _createSizes(_kItemCount).toList(),
+        super(key: key);
 
   Size deviceSize;
+
+  static const int _kItemCount = 1000;
+  final List<IntSize> _sizes;
 
   @override
   Widget build(BuildContext context) {
     deviceSize = MediaQuery.of(context).size;
-
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(5),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FadeInImage.memoryNetwork(
-              placeholder: kTransparentImage,
-              image:
-                  'http://ztd00.photos.bdimg.com/ztd/w=700;q=50/sign=cf3f1428a1ec8a13141a55e0c738e0b2/dc54564e9258d1092bcd9975d958ccbf6d814dce.jpg',
-              width: deviceSize.width * 0.5 - 10,
-              fit: BoxFit.fitWidth),
-          /*2*/
-          Container(
-            padding: const EdgeInsets.only(bottom: 8),
-            child: Text(
-              'Oeschinen Lake Campground',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Text(
-            'Kandersteg, Switzerland',
-            style: TextStyle(
-              color: Colors.grey[500],
-            ),
-          ),
-        ],
-      ),
-    );
-
-    List<Widget> leftRightColumns(List<Attraction> attractions) {
-      final leftColumn = Column(
-        children: [titleSection, titleSection],
-      );
-      final rightColumn = Column(children: [titleSection]);
-      leftColumn.children.add(titleSection);
-
-      leftColumn.children.addAll([
-        titleSection,
-        titleSection,
-      ]);
-      rightColumn.children.addAll([
-        titleSection,
-      ]);
-      return [leftColumn, rightColumn];
-    }
 
 //    return Scaffold(
 //      //search bar ref: https://github.com/rodolfoggp/search_app_bar
@@ -105,97 +66,171 @@ class PeripheryBody extends StatelessWidget {
 //          )),
 //    );
 
-    return Scaffold(
-      //search bar ref: https://github.com/rodolfoggp/search_app_bar
-      appBar: AppBar(title: Text('trip attraction')),
-      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
-        return Container(
-
-            padding: const EdgeInsets.all(5.0),
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-              Column(
+//    return Scaffold(
+//      //search bar ref: https://github.com/rodolfoggp/search_app_bar
+//      appBar: AppBar(title: Text('trip attraction')),
+//      body: ListView.builder(itemBuilder: (BuildContext context, int index) {
+//        return Container(
+//
+//            padding: const EdgeInsets.all(5.0),
+//            child: Row(
 //                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/232/232824277.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494929.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494172.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/330/33048216.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/179/179989150.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/179/179990422.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                ],
+//                children: [
+//              Column(
+////                crossAxisAlignment: CrossAxisAlignment.start,
+//                children: [
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/232/232824277.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  SizedBox(
+//                    height: 5.0,
+//                  ),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494929.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  SizedBox(
+//                    height: 5.0,
+//                  ),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494172.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/330/33048216.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/179/179989150.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/179/179990422.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                ],
+//              ),
+//              SizedBox(
+//                width: 10.0,
+//              ),
+//              Column(
+//                children: [
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494929.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  SizedBox(
+//                    height: 5.0,
+//                  ),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494172.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  SizedBox(
+//                    height: 5.0,
+//                  ),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/330/33048216.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/179/179989150.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                  FadeInImage.assetNetwork(
+//                      placeholder: 'assets/flutter_logo.png',
+//                      image:
+//                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/179/179990422.jpg',
+//                      width: deviceSize.width * 0.5 - 10),
+//                ],
+//              )
+//            ]));
+//      }),
+//    );
+
+//    https://github.com/letsar/flutter_staggered_grid_view
+
+    return Scaffold(
+        appBar: AppBar(title: Text('trip attraction')),
+        body: StaggeredGridView.countBuilder(
+          padding: const EdgeInsets.all(5.0),
+          primary: false,
+          crossAxisCount: 4,
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          itemBuilder: (context, index) => new _Tile(index, _sizes[index]),
+          staggeredTileBuilder: (index) => new StaggeredTile.fit(2),
+        ));
+  }
+}
+
+List<IntSize> _createSizes(int count) {
+  Random rnd = new Random();
+  return new List.generate(count,
+          (i) => new IntSize((rnd.nextInt(500) + 200), rnd.nextInt(800) + 200));
+}
+
+class IntSize {
+  const IntSize(this.width, this.height);
+
+  final int width;
+  final int height;
+}
+
+class _Tile extends StatelessWidget {
+  const _Tile(this.index, this.size);
+
+  final IntSize size;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      child: new Column(
+        children: <Widget>[
+          new Stack(
+            children: <Widget>[
+              //new Center(child: new CircularProgressIndicator()),
+              new Center(
+                child: new FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: 'https://picsum.photos/${size.width}/${size.height}/',
+                ),
               ),
-              SizedBox(
-                width: 10.0,
+              new Text(
+                'Image number $index',
+                style: const TextStyle(fontWeight: FontWeight.bold),
               ),
-              Column(
-                children: [
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494929.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/245/245494172.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/330/33048216.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-q.static.booking.cn/images/hotel/max1280x900/179/179989150.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                  FadeInImage.assetNetwork(
-                      placeholder: 'assets/flutter_logo.png',
-                      image:
-                          'https://ac-r.static.booking.cn/images/hotel/max1280x900/179/179990422.jpg',
-                      width: deviceSize.width * 0.5 - 10),
-                ],
-              )
-            ]));
-      }),
+            ],
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: new Column(
+              children: <Widget>[
+                new Text(
+                  'Width: ${size.width}',
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                new Text(
+                  'Height: ${size.height}',
+                  style: const TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
